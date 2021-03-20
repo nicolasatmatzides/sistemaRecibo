b43a0c5b32836ac3321f4029abd5edadc28ec71d
Nicolas Atmatzides <nicolas.atmatzides@marlo.com.au>
2021-03-20 14:55:48 -0300

    started spring boot

--- src/main/java/com/uff/entity/Imovel.java
+++ src/main/java/com/uff/entity/Imovel.java
@@ -1,4 +1,22 @@
 package com.uff.entity;
 
-public class Imovel {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Imovel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
